438929e1ec6f2419d9bbe47cc7da520f2282a107
qppon <f0x5erg@yandex.ru>
2024-10-15 15:59:20 +0300

    sprint_07

---
+++ MovieQuizTests/MovieQuizTests.swift
@@ -0,0 +1,49 @@
+//
+//  MovieQuizTests.swift
+//  MovieQuizTests
+//
+//  Created by Jojo Smith on 10/15/24.
+//
+
+import XCTest
+
+struct ArithmeticOperations {
+    func addition(num1: Int, num2: Int, handler: @escaping (Int) -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            handler(num1 + num2)
+        }
+    }
+    
+    func subtraction(num1: Int, num2: Int, handler: @escaping (Int) -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            handler(num1 - num2)
+        }
+        
+    }
+    
+    func multiplication(num1: Int, num2: Int, handler: @escaping (Int) -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            handler(num1 * num2)
+        }
+    }
+}
+
+class MovieQuizTests: XCTestCase {
+
+    func testAdiction() throws {
+        let arithmeticOperations = ArithmeticOperations()
+        let num1 = 1
+        let num2 = 2
+        
+        let expectation = expectation(description: "Addition function expectation")
+        
+        
+        let result = arithmeticOperations.addition(num1: num1, num2: num2) { result in
+            XCTAssertEqual(result, 4)
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 2)
+    }
+
+}
